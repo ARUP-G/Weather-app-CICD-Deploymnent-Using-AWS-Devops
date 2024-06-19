3e442b41950b7291d4f631def163169709ad1eb4
Arup <darup2019@gmail.com>
2024-06-19 12:35:08 +0530

    docker push command is changed to docker pull command
    closing #8

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push ard3dk/weather-app:1.1
+docker pull ard3dk/weather-app:1.1
 
 # Run the Docker image as a container
 docker run -d -p 8080:8080 ard3dk/weather-app:1.1
+
